a8a109cd6a6125c41c13d53d1f6c59454b383b75
dumh <dumh>
2022-11-10 01:07:29 +0800

    1. fix statusline component scrollbar bug

--- nvim/lua/lualine/components/file_scrollbar.lua
+++ nvim/lua/lualine/components/file_scrollbar.lua
@@ -16,7 +16,9 @@ function M:update_status()
 
     local curr_line = vim.api.nvim_win_get_cursor(0)[1]
     local lines = vim.api.nvim_buf_line_count(0)
-
+    if lines == 0 then
+        return ""
+    end
     if self.options.reverse then
         return string.rep(scrollbar_blocks[8 - math.floor(curr_line / lines * 7)], 2)
     else
